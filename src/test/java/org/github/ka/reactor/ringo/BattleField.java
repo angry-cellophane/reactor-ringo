8a13fbdce2122cd99d372b2e7e7a1e801c900b87
Alex Kazakov <kazakov.a.s.90@gmail.com>
2021-08-22 23:56:48 +0100

    Testing produce-consumer

--- src/test/java/org/github/ka/reactor/ringo/BattleField.java
+++ src/test/java/org/github/ka/reactor/ringo/BattleField.java
@@ -1,10 +1,18 @@
 package org.github.ka.reactor.ringo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Sinks;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class BattleField {
 
     @Test
@@ -24,5 +32,36 @@ public class BattleField {
                     .verify();
             System.out.println("done");
         });
+    }
+
+    @Test
+    void test2() throws InterruptedException {
+        int parallelLevel = 1;
+        var consumerScheduler = Schedulers.newParallel("consumers", parallelLevel);
+        var producerScheduler = Schedulers.newSingle("producer");
+
+        Flux.<Integer>create(sink -> {
+            sink.onRequest(n -> {
+                log.info(LocalDateTime.now() + "requested " + n + " elements");
+                producerScheduler.schedule(() -> {
+                    for (int i=0; i<n; i++) {
+                        sink.next(1);
+                    }
+                });
+            });
+        })
+        .limitRate(parallelLevel)
+        .parallel(parallelLevel)
+        .runOn(consumerScheduler)
+        .subscribe(n -> {
+            log.info(LocalDateTime.now() + " received number " + n);
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (Exception e) {
+                //
+            }
+        });
+
+        TimeUnit.SECONDS.sleep(30);
     }
 }
